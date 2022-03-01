b800e3aae1f842436f50ac467572a0b48d8d2762
asdone7 <pnxsx@hotmail.com>
2022-03-01 10:41:48 +0800

    Merge Pull origin-tracking branch 'https://github.com/asdone7/emuelec' into 4.4-dev

--- packages/python/devel/Jinja2/package.mk
+++ packages/python/devel/Jinja2/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Jinja2"
-PKG_VERSION="3.0.3"
-PKG_SHA256="611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+PKG_VERSION="3.0.x"
+PKG_SHA256="66c6d2140b1c92bade3b4b9423b5a6ce0967a4e68dcb4d76bfd67f1a0807d2d9"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.org/project/Jinja2/"
-PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/pallets/jinja/archive/bb7f3a51091d91010deb1e01e69f4f255d3b3eb6.tar.gz"
 PKG_DEPENDS_HOST="Python3:host setuptools:host MarkupSafe:host"
 PKG_LONGDESC="Jinja is a fast, expressive, extensible templating engine."
 PKG_TOOLCHAIN="manual"
--- packages/sx05re/emulators/fbneoSA/package.mk
+++ packages/sx05re/emulators/fbneoSA/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="fbneoSA"
-PKG_VERSION="54628bdbc4dcdce06e770ed738c7ce9a18ce3a65"
+PKG_VERSION="f8a4cf80315ac01f6ef0077c5b551c993143467e"
 PKG_ARCH="aarch64"
 PKG_LICENSE="Custom"
 PKG_SITE="https://github.com/finalburnneo/FBNeo"
