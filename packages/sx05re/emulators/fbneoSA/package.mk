006cffeb8a80bd7bd94d2e1061e3b072cce1fb55
asdone7 <pnxsx@hotmail.com>
2022-03-01 12:18:23 +0800

    Merge Pull origin-tracking branch 'https://github.com/asdone7/emuelec' into 4.4-dev

--- packages/sx05re/emulators/fbneoSA/package.mk
+++ packages/sx05re/emulators/fbneoSA/package.mk
@@ -28,4 +28,6 @@ mkdir -p $INSTALL/usr/bin
 cp -rf ${PKG_BUILD}/fbneo $INSTALL/usr/bin
 cp -rf ${PKG_BUILD}/src/license.txt $INSTALL/usr/bin/fbneo_license.txt
 cp -rf ${PKG_DIR}/scripts/* $INSTALL/usr/bin
+
+chmod +x $INSTALL/usr/bin/fbneo.sh
 }
