b3e74c1a0f375c64b4d60f6de329bdbaaa54e3a0
asdone7 <pnxsx@hotmail.com>
2022-03-06 23:06:40 +0800

    Merge branch 'EmuELEC' into dev

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="9edf4d5b6706c462201e89111e356c64287f8057"
+PKG_VERSION="fa92895db0bae0cca7c91c18d8502460bb90a8cf"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
